fa62b4346bce925745d2bd5d42a80dda3e7bab33
Raj Kumar Goyal <goyalkrraj@gmail.com>
2018-12-14 12:30:51 +0530

    Convert phone number as key / value pair. value is the possible characters array corresponding to phone number key

--- conversion.rb
+++ conversion.rb
@@ -4,10 +4,10 @@ require 'pry'
 # Convert phone number to meaningfull word(s) based on available dictionary.
 # Meaningfull words help to remember phone number.
 class Conversion
-  attr_reader :number
+  attr_reader :phone_number
 
-  def initialize(number)
-    @number = number
+  def initialize(phone_number)
+    @phone_number = phone_number
     @digit_to_chars = { '2' => %w[a b c],
                         '3' => %w[d e f],
                         '4' => %w[g h i],
@@ -16,18 +16,29 @@ class Conversion
                         '7' => %w[p q r s],
                         '8' => %w[t u v],
                         '9' => %w[w x y z] }
+    dictionary = []
+    File.foreach( "dictionary.txt" ) do |word|
+      dictionary << word.strip
+    end
   end
 
   def valid_phone_number?
-    p number
-    return false if number.nil?
+    p phone_number
+    return false if phone_number.nil?
 
-    number.gsub!(/[^2-9]/, '') # select digits only 2 to 9. Ignore 0 and 1
-    p number
-    p number.length
-    return true if number.length == 10
+    phone_number.gsub!(/[^2-9]/, '') # select digits only 2 to 9. Ignore 0 and 1
+    p phone_number
+    p phone_number.length
+    return true if phone_number.length == 10
+  end
+  
+  def extract_words
+    phone_keys = phone_number.chars.map{|n| @digit_to_chars[n] }
+    binding.pry
   end
 end
 object = Conversion.new('6686787825')
 p 'is valid phone number?'
 p object.valid_phone_number?
+object.extract_words
+
