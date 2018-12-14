346cb412d2de853f7dda58aa459b7edfd800fb7e
Raj Kumar Goyal <goyalkrraj@gmail.com>
2018-12-14 11:44:36 +0530

    Created hash as defind in puzzle

--- conversion.rb
+++ conversion.rb
@@ -8,6 +8,14 @@ class Conversion
 
   def initialize(number)
     @number = number
+    @digit_to_chars = { '2' => %w[a b c],
+                        '3' => %w[d e f],
+                        '4' => %w[g h i],
+                        '5' => %w[j k l],
+                        '6' => %w[m n o],
+                        '7' => %w[p q r s],
+                        '8' => %w[t u v],
+                        '9' => %w[w x y z] }
   end
 
   def valid_phone_number?
